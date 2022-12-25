60eaf31097aac8112da57f17c86e401965331106
ckwatt11 <ckwatt11@gmail.com>
2022-12-25 14:00:13 -0500

    get basic weather info

--- cli-weather.py
+++ cli-weather.py
@@ -7,7 +7,7 @@ from argparse import *
 
 API_ACCESS_LINK = "http://api.openweathermap.org/data/2.5/weather" # appended content to link will be user input that will build the endpoint query. 
 
-#  API_FORECAST_ACCESS_LINK = "https://pro.openweathermap.org/data/2.5/forecast/hourly?{}{}&appid={API key}".format("lat={}&".format(), "lon={}&".format())
+#  API_FORECAST_ACCESS_LINK = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
 
 
 # create getter for personal API key.
@@ -23,21 +23,23 @@ def getApiKey():
 # create function for parsing args passed to CLI
 def parse_args():
 
-    argParser = ArgumentParser(description= "Return weather/forecast for the near future (~4 days) for a city.")
+    argParser = ArgumentParser(description= "Return weather/forecast for the near future (~5 days) for a city.")
     argParser.add_argument("city", nargs="+", type=str, help="entered city name must not contain spelling errors") # allows user to pass multiple whitespace-separated words as city names
     argParser.add_argument("-i", "--imperial", action="store_true", help="change display mode to Fahrenheit.")
+    #argParser.add_argument("-f", "--forecast", action="store_true", help="show 5 day/3 hour forecast for the city") 
     return argParser.parse_args()
 
 
-def constructRequest(city_name, isImperial=False, isForecast=False):
+def constructRequest(city_name, country_code, isImperial=False, isForecast=False):
      
     """ 
-    Combine the components back into a URL string, and to convert a “relative URL” to an absolute URL given a “base URL.”
+    Combine the components back into a URL API request string, and to convert a “relative URL” to an absolute URL given a “base URL.”
 
     """
     appId = getApiKey()
     city = " ".join(city_name)
     cityNameUrl = parse.quote_plus(city) # how city names (should) appear in the URL : 'Sao Paulo' -> 'Sao+Paulo' 
+    # forecast = 
     units = "imperial" if isImperial else "metric"
     apiRequest = API_ACCESS_LINK + "?q={}".format(cityNameUrl) + "&units={}".format(units) + "&appid={}".format(appId)
     return apiRequest
@@ -46,19 +48,24 @@ def constructRequest(city_name, isImperial=False, isForecast=False):
 
 def fetchWeather(req):  # return weather data based on the constructed API request URL
 
+    '''
+    req -> constructed API request
+    '''
+
     # add error/exception handling for conciseness and readability
     try:
         result = request.urlopen(req) # create GET request
     except error.HTTPError as http_err:
         match http_err:
-            case int('401'):
+            case '401':
                 sys.exit("Access not authorized. Please check your API key.")
-            case int('404'): 
+            case '404': 
                 sys.exit("No weather information available for the city: {}".format(args_passed.city))
             case default:
-                sys.exit("You encountered an error: {}".format(http_err))    
+                sys.exit("The app encountered an error: {}".format(http_err))    
 
     weatherInfo = result.read()
+
     try:
         return json.loads(weatherInfo)
     except json.JSONDecodeError:
@@ -73,7 +80,7 @@ if __name__ == "__main__":
     args_passed = parse_args()
     print(args_passed.city, args_passed.imperial)
     cityWeather = fetchWeather(constructRequest(args_passed.city, args_passed.imperial))
-    print(cityWeather)
+    pprint.pp(cityWeather)
     
     
     
